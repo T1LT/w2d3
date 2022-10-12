ba6d30b04e47c7d7557c8e6e74c844f58b94f739
Nishant Racherla <nishant.racherla@gmail.com>
2022-10-11 22:08:54 -0700

    mastermind done

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -5,5 +5,62 @@ class Code
     "B" => :blue,
     "Y" => :yellow
   }
+  attr_reader :pegs
+  def initialize(pegs)
+    if !Code.valid_pegs?(pegs)
+      raise "invalid pegs"
+    end
+    @pegs = pegs.map!(&:upcase)
+  end
+
+  def self.valid_pegs?(pegs)
+    pegs.all? { |el| POSSIBLE_PEGS.keys.include? el.upcase }
+  end
+
+  def self.random(length)
+    res = Array.new(length, "")
+    res.map! { |el| el = POSSIBLE_PEGS.keys.sample }
+    Code.new(res)
+  end
+
+  def self.from_string(str)
+    Code.new(str.split(""))
+  end
+
+  def [](idx)
+    @pegs[idx]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(code)
+    count = 0
+    (0...length()).each do |idx|
+      count += 1 if @pegs[idx] == code[idx]
+    end
+    count
+  end
+
+  def num_near_matches(code)
+    codecopy = []
+    pegscopy = []
+    (0...length()).each do |idx|
+      if @pegs[idx] != code[idx]
+        codecopy << code[idx]
+        pegscopy << @pegs[idx]
+      end
+    end
+    (codecopy & pegscopy).length
+  end
+
+  def ==(code)
+    return false if code.length != self.length()
+    @pegs.each_with_index do |peg, idx|
+      return false if code[idx] != peg
+    end
+    true
+  end
 
 end
--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+    def initialize(length)
+        @secret_code = Code.random(length)
+    end
 
+    def print_matches(code)
+        puts @secret_code.num_exact_matches(code)
+        puts @secret_code.num_near_matches(code)
+    end
+
+    def ask_user_for_guess
+        puts "Enter a code"
+        input = gets.chomp
+        input = Code.from_string(input)
+        self.print_matches(input)
+        @secret_code == input
+    end
 end
